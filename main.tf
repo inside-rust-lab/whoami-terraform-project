7087ffe46666edd44088e4f9d2f7c7f1c91b7c39
“eddie” <eddie.eby@gmail.com>
2026-07-31 18:48:27 -0600

    add private route table and nat gateway

--- main.tf
+++ main.tf
@@ -101,3 +101,41 @@ resource "aws_route_table_association" "public-b" {
 
   route_table_id = aws_route_table.public.id
 }
+
+resource "aws_nat_gateway" "main" {
+  vpc_id            = aws_vpc.main.id
+  availability_mode = "regional"
+
+  tags = {
+    Name = "regional-nat-gateway"
+  }
+
+  depends_on = [
+    aws_internet_gateway.main
+  ]
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.main.id
+  }
+
+  tags = {
+    Name = "private-route-table"
+  }
+}
+
+resource "aws_route_table_association" "private-a" {
+  subnet_id = aws_subnet.private-a.id
+
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private-b" {
+  subnet_id = aws_subnet.private-b.id
+
+  route_table_id = aws_route_table.private.id
+}
