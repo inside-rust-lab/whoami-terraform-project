7cc8962cf3ed1741fe7ff1eae72c296a048924ab
“eddie” <eddie.eby@gmail.com>
2026-08-01 15:46:52 -0600

    add security groups

--- main.tf
+++ main.tf
@@ -2,6 +2,8 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# VPC
+
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 
@@ -139,3 +141,72 @@ resource "aws_route_table_association" "private-b" {
 
   route_table_id = aws_route_table.private.id
 }
+
+# SECURITY GROUPS
+
+resource "aws_security_group" "alb" {
+  name        = "alb-security-group"
+  description = "Allow web traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "HTTP"
+
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "alb-security-group"
+  }
+}
+
+resource "aws_security_group" "ec2" {
+  name        = "ec2-security-group"
+  description = "Allow HTTP traffic from ALB"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "HTTP"
+
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+
+    security_groups = [aws_security_group.alb.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ec2-security-group"
+  }
+}
+
